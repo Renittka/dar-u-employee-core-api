8ab58825d0bf07847c52c50e0a563c499cf8edf8
Anna <renita5701@gmail.com>
2023-02-11 12:05:39 +0600

    Changed controller

--- src/main/java/kz/dar/university/employeecoreapi/controller/EmployeeController.java
+++ src/main/java/kz/dar/university/employeecoreapi/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package kz.dar.university.employeecoreapi.controller;
 import kz.dar.university.employeecoreapi.model.EmployeeModel;
 import kz.dar.university.employeecoreapi.service.IEmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -14,6 +15,16 @@ public class EmployeeController {
 
     @Autowired
     private IEmployeeService employeeService;
+
+    @Autowired
+    Environment env;
+
+    @GetMapping("/check")
+    public String check() {
+
+        return "employee-core-api is working at the port " + env.getProperty("local.server.port");
+
+    }
 
     @PostMapping
     public void createEmployee(@RequestBody @Valid EmployeeModel employee) {
